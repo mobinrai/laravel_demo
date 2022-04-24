4ef53024f5c2e301fe35f0010bc1b5a4d9a1ea11
mobinrai <mobinraee@gmail.com>
2022-04-24 18:27:47 +0200

    added views for front view

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+require __DIR__.'/front.php';
 require __DIR__.'/admin.php';
-
  // require __DIR__.'/user.php';
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
